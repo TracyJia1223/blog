4421a0e1977d43d3553995665812f08dd4b62f24
Tracy <tracy.jia1223@gmail.com>
2016-10-26 17:17:43 -0700

    changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: {case_sensitive: false, message: 'must be unique'}
   validates :body, length: { minimum: 5 }
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "30x30#"}
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "200x200#"}
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
